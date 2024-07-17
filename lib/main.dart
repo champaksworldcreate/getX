0c97b93cf7ceb8e1885bf8401a66098ac1a87805
champaksworldcreate <champaksworld@gmail.com>
2024-07-17 13:22:22 +0530

    Add GetMaterialApp in our app

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( GetMaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
