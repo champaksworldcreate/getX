1e94456ac2bef53cca85f48a0873d3e9f1651303
champaksworldcreate <champaksworld@gmail.com>
2024-08-17 14:08:23 +0530

    Back Up

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,71 @@
-// lib/main.dart
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:getx/secondpage.dart';
-
-import 'controller.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Hello World App',
-      home: MyHomePage(),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: UnlimitedList(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final CounterController controller = Get.put(CounterController());
+class UnlimitedList extends StatefulWidget {
+  const UnlimitedList({super.key});
+
+  @override
+  UnlimitedListState createState() => UnlimitedListState();
+}
+
+class UnlimitedListState extends State<UnlimitedList> {
+  final List<int> _items = List.generate(20, (index) => index);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('GetX Counter'),
+        title: const Text('Infinite Scroll Grid'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-        const    Text(
-              'GetX Example',
-            ),
-            Obx(() => Text('Last Activity: ${controller.lastoperation}     ${controller.count.value}',
-                style: Theme.of(context).textTheme.headlineMedium)),
-            Obx(() => Text('${controller.count.value}',
-                style: Theme.of(context).textTheme.headlineMedium)),
-            ElevatedButton(
-              onPressed: controller.increment,
-              child:const Text('Increment'),
-            ),
-            ElevatedButton(
-              onPressed: controller.decrement,
-              child:const Text('Decrement'),
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            InkWell(
-              child: Container(
-                height: 50,
-                width: 100,
-                color: Colors.blue,
-                child: Center(
-                  child: Text("Click"),
-                ),
+      body: NotificationListener<ScrollNotification>(
+        onNotification: (ScrollNotification scrollInfo) {
+          if (scrollInfo.metrics.pixels == scrollInfo.metrics.maxScrollExtent) {
+            _loadMore();
+          }
+          return false;
+        },
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+          itemCount: _items.length,
+          itemBuilder: (context, index) {
+            return Container(
+              alignment: Alignment.center,
+              color:(index %2==0)? Colors.teal:Colors.brown,
+              child: Text(
+                'Item ${_items[index]}',
+                style: const TextStyle(fontSize: 16),
               ),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SecondPage()),
-                );
-              },
-            )
-          ],
+            );
+          },
         ),
       ),
+    );
+  }
+
+  void _loadMore() {
+    setState(
+      () {
+        _items.addAll(List.generate(20, (index) => _items.length + index));
+      },
     );
   }
 }
